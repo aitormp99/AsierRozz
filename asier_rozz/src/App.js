327b18073dcbfa7621da72a71bf498d1797b3dcb
aitormp99 <aitormp99@gmail.com>
2021-01-08 01:16:34 +0100

    principio de la pagina web

---
+++ asier_rozz/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <img class="logo" src={"/AR_logo_pequeño.png"} />
+        <p>
+        El DJ de confianz
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
